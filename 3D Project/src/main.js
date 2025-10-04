bb82796eeb4b98bca651ffd96d8515abe8eb2824
AkhmadRafiHaidarrahman <nakanom858@gmail.com>
2025-10-04 18:15:27 +0700

    update

--- 3D Project/src/main.js
+++ 3D Project/src/main.js
@@ -6,7 +6,6 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const renderer = new THREE.WebGLRenderer();
-render.setAnimationLoop( animate )
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
@@ -23,6 +22,9 @@ const controls = new OrbitControls( camera, renderer.domElement );
 function animate() {
     renderer.render( scene, camera );
     cube.rotation.x += 0.01;
-    cube.rotation.y += 0.07;
+    
+    cube.rotation.y += 0.01;
 }
+renderer.setAnimationLoop( animate )
+
 renderer.setAnimationLoop( animate );
