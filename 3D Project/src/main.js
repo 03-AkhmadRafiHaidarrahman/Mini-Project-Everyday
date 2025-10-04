de947637a2e8528472cfd75be214906650bf4c00
AkhmadRafiHaidarrahman <nakanom858@gmail.com>
2025-10-04 18:41:28 +0700

    update

--- 3D Project/src/main.js
+++ 3D Project/src/main.js
@@ -10,14 +10,30 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const material = new THREE.MeshBasicMaterial( {
+  color: 0xFF8844, 
+  // map: texture,
+  } );
+const loader = new THREE.TextureLoader();
+const texture = loader.load( 'kristin-o-karlsen-BfqbYyfo56w-unsplash.jpg' );
+texture.colorSpace = THREE.SRGBColorSpace;
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.z = 5;
 
 
+
+
+
 const controls = new OrbitControls( camera, renderer.domElement );
+controls.target.set(0, 0, 0);
+controls.update();
+
+const color = 0xFFFFFF;
+const intensity = 1;
+const light = new THREE.AmbientLight(color, intensity);
+scene.add(light);
 
 function animate() {
     renderer.render( scene, camera );
@@ -26,5 +42,3 @@ function animate() {
     cube.rotation.y += 0.01;
 }
 renderer.setAnimationLoop( animate )
-
-renderer.setAnimationLoop( animate );
