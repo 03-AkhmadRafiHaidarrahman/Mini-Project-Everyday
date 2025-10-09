f5f7dd0a3820109075c405bc3d80d36e39926b8e
AkhmadRafiHaidarrahman <nakanom858@gmail.com>
2025-10-09 22:12:05 +0700

    update ilmu

--- Costum Regex Engine/script.js
+++ Costum Regex Engine/script.js
@@ -6,27 +6,27 @@ My Purpose:
 
 // Still In Learning Not Finish!!
 
-function recognize(string1,string2){
-    // if(!string1) return true
+
+ // if(string1 !== string2) return true
+        // if(!string1[i]) return true
+        // if(!string2) return false
+        // if(string1 === ".") return true
+        // return string1 === string2
+
+// if(!string1) return true
     // if(!string2) return false
     // if(string1 === ".") return true
     // return string1 === string2
     // return string1 === string2
 
-    if(string1.length !== string2.length) return false
 
+function recognize(string1,string2){
+    if(string1.length !== string2.length) return false
     for (let i = 0; i < string1.length; i++) {
         if(string1[i] === ".") continue
         if(string1[i] !== string2[i])return false
-        // if(string1 !== string2) return true
-        // if(!string1[i]) return true
-        // if(!string2) return false
-        // if(string1 === ".") return true
-        // return string1 === string2
     }
-
-        return true
-    
+    return true
 }
 
 console.log(recognize("a","z"))
@@ -34,7 +34,18 @@ console.log(recognize("a","a"))
 console.log(recognize("a",1))
 console.log(recognize("a",""))
 console.log(recognize("a","."))
+console.log(recognize("a.","ac"))
 console.log(recognize(".","z"))
+console.log(`==================================`)
+function match(teks1,teks2){
+    if(teks1.length !== teks2.length) return false
+    for (let i = 0; i < teks1.length,teks2.length; i++) {
+        if(teks1[i],teks2[i])        
+    }
+}
+console.log(match("a*b", "b"))
+console.log(match("a*b", "ab"))
+console.log(match ("a*b", "aaab"))
 
 // function match(string1,string2){
 //     if(string1 === "")return true
