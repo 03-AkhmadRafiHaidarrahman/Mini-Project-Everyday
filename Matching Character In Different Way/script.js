dbdb8d7bb3b2e9e28fd888b71d97bef508a5cf29
AkhmadRafiHaidarrahman <nakanom858@gmail.com>
2025-10-14 20:24:53 +0700

    Update For Email Input

--- Matching Character In Different Way/script.js
+++ Matching Character In Different Way/script.js
@@ -14,22 +14,49 @@ button.addEventListener("click",function(){
         Swal.fire("End Character With Number!");
     }
 })  
+
 let EmailValid = document.getElementById("EmailValid")
 let InputEmail = document.getElementById("Input-Email")
 let BtnEmail = document.getElementById("BtnEmail")
+let Warn = document.getElementById("WarningMsg")
 
-BtnEmail.addEventListener("click",function(){
-let regex1 = /\w{1,4}\d(@gmail.com)$/
-let regex2 = /\w{5,7}\d(@gmail.com)$/
-let regex3 = /\w{8,}\d(@gmail.com)$/
+BtnEmail.addEventListener("click",function(){  
+let regex1 = /^\w{1,4}\d(@gmail.com)$/
+let regex2 = /^\w{5,7}\d(@gmail.com)$/
+let regex3 = /^\w{8,}\d(@gmail.com)$/
 let value = InputEmail.value
+
 if(regex1.test(value)){
     alert("Worst Email")
-}if(regex2.test(value)){
+    let li1 = document.createElement("li")
+    li1.textContent = `Worst Email`
+    Warn.appendChild(li1)
+    setTimeout(() => {
+        Warn.removeChild(li1)
+    }, 1000);
+}
+if(regex2.test(value)){
     alert("Bad Email")
+    let li2 = document.createElement("li")
+    li2.textContent = `Bad Email`
+    Warn.appendChild(li2)
+    setTimeout(() => {
+        Warn.removeChild(li2)
+    }, 1000);
 }if(regex3.test(value)){
     alert("Good Email")
+    let li3 = document.createElement("li")
+    li3.textContent = `Good Email`
+    Warn.appendChild(li3)
+    setTimeout(() => {
+        Warn.removeChild(li3)
+    }, 1000);
 }
 })
 
+const UrlInput = document.getElementById("UrlInput")
+const UrlBtn = document.getElementById("UrlBtn")
 
+UrlBtn.addEventListener("click",function(){
+    
+})
