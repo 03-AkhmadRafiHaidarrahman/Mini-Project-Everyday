c190345ddf9b3e6203ad73bc4a94134a8adc8ff5
AkhmadRafiHaidarrahman <nakanom858@gmail.com>
2025-10-22 22:41:54 +0700

    Word Countergit add . Yappie

---
+++ Word Counter/script.js
@@ -0,0 +1,13 @@
+let digit = document.getElementById("digit")
+let Input = document.getElementById("input")
+let clear = document.getElementById("clear")
+
+Input.addEventListener("input",function(){
+    let length = Input.value.length
+    console.log(length)
+    let max = Input.maxLength    
+    digit.textContent = `${length} / ${max}`
+    clear.style.background = length >= 100 ? 'red' : 'black'
+    console.log(max)
+
+})
