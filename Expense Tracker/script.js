c1f3e58aabae4629039d41ed8ec6ae788b379980
AkhmadRafiHaidarrahman <nakanom858@gmail.com>
2025-10-25 15:45:16 +0700

    Expence Tracker Boy

--- Expense Tracker/script.js
+++ Expense Tracker/script.js
@@ -1,6 +1,43 @@
+let balance = document.getElementById("balance")
+
+let Transaction = document.getElementById("Transaction")
+
+let Amount = document.getElementById("Amount")
+let BtnSub = document.getElementById("BtnSub")
+
+let History1 = document.getElementById("history")
+
+function amount(){
+    let value = Amount.value
+    return value
+}
+
+function clear(){
+    Transaction.value = ""
+    Amount.value = ""
+}
+
+function transaction(){
+    let value = Transaction.value
+    return value
+}
 
 
+function history(){
 
+}
+
+
+BtnSub.addEventListener("click",function(event){
+    let li = document.createElement("li")
+    li.innerHTML = `${transaction()} <p>${amount()}</p>`
+    balance.textContent = amount()
+    if(li){
+        clear()
+    }
+    History1.appendChild(li)
+    event.preventDefault()
+})
 
 
 
