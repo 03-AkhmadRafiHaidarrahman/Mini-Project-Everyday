a2e6ca5913fd587bc418317028a7010a8a9368d5
AkhmadRafiHaidarrahman <nakanom858@gmail.com>
2025-11-03 21:34:17 +0700

    new

---
+++ Palindrome Checker/script.js
@@ -0,0 +1,13 @@
+// 💡 Goal of the Project
+
+// The project’s main goal is to:
+
+// Take input from the user (like a word or sentence).
+
+// Process the string (remove spaces, punctuation, and make it lowercase).
+
+// Reverse it.
+
+// Compare the original with the reversed version.
+
+// Return whether it’s a palindrome or not.
