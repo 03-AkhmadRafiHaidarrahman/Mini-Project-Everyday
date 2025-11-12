b16ac21e76ca688cc5c443a38851c9d9e6ae2b0d
AkhmadRafiHaidarrahman <nakanom858@gmail.com>
2025-11-12 23:04:46 +0700

    work but not done

--- Coin Flip/script.js
+++ Coin Flip/script.js
@@ -2,14 +2,21 @@ const flip = document.getElementById("flip")
 const coin = document.getElementById("coin")
 
 const arrimg = ["palarupiah-removebg-preview.png","rupiah-removebg-preview.png"]
-let index = 0 
+let time;
 
 flip.addEventListener("click",function(){
+   time = setInterval(() => {
     coin.style.height = "0rem"
     setTimeout(() => {
-    index = (index + 1) % arrimg.length
-    coin.src = arrimg[index]
-    coin.style.height = "10rem"
-    }, 500);
+    const random = arrimg[Math.floor(Math.random() * arrimg.length)] 
+    coin.src = random    
+    console.log(random)
+    console.log(coin.src)
+    coin.style.height = "8rem"
+    }, 500);        
+    }, 300);
 
+    setTimeout(() => {
+        clearInterval(time)
+    }, 3000);
 })
